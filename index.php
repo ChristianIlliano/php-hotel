cbec1303afcffaabcc1b623890367bc2462a27ea
ChristianIlliano <grafica.illiano@gmail.com>
2024-05-21 17:58:01 +0200

    feat: table and logic

--- index.php
+++ index.php
@@ -52,47 +52,67 @@ $hotels = [
     <?php
     $parcking = $_GET["parcking"];
     $votation = $_GET["votation"];
-    var_dump($_GET)
+    // var_dump($_GET)
     ?>
 
     <form method="GET">
-        <fieldset class="mb-3 container">
+        <fieldset class="mb-3 container ">
             <h2>Dati Hotels</h2>
             <div class="mb-3">
                 <label for="disabledSelect" class="form-label">Disponibilità parcheggio Obbligatoria</label>
                 <select id="disabledSelect" name="parcking" class="form-select">
-                    <option value="false">SI</option>
-                    <option value="true">NO</option>
+                    <option value="true">SI</option>
+                    <option value="false">NO</option>
                 </select>
             </div>
             <div class="mb-3">
                 <label for="disabledSelect" class="form-label">Voto</label>
                 <select id="disabledSelect" name="votation" class="form-select">
-                    <option value="A">1</option>
-                    <option value="B">2</option>
-                    <option value="C">3</option>
-                    <option value="D">4</option>
-                    <option value="E">5</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
                 </select>
             </div>
             <button type="submit" class="btn btn-primary">Cerca</button>
         </fieldset>
     </form>
+    <div class="mb-3">
+        <table class="table container">
 
-    <?php foreach ($hotels as $hotel_index => $info_hotel) { ?>
-        <div class="mb-3 container">
-            <?php
-            if ($_GET["parcking"] === 'true' || $info_hotel["parking"] === true) { ?>
-                <div>
-                    <h4><?php echo $info_hotel['name']; ?></h4>
-                    <p>Descrizione: <?php echo $info_hotel["description"]; ?></p>
-                    <p>Voto: <?php echo $info_hotel["vote"]; ?></p>
-                    <p>Distanza dal centro: <?php echo $info_hotel["distance_to_center"]; ?></p>
-                </div>
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <?php foreach ($hotels as $hotel_index => $info_hotel) { ?>
+                        <?php
+                        //var_dump($votation, $info_hotel["vote"]);
+                        if ($parcking  === 'true' && $info_hotel["parking"] == true && $info_hotel["vote"] >= $votation) { ?>
+                <tr>
+                    <th scope="row"><?php echo $info_hotel['name']; ?></th>
+                    <td><?php echo $info_hotel["description"]; ?></td>
+                    <td> <?php echo $info_hotel["vote"]; ?></td>
+                    <td><?php echo $info_hotel["distance_to_center"]; ?></td>
+                </tr>
+            <?php } elseif ($info_hotel["parking"] === false && $info_hotel["vote"] >= $votation) { ?>
+                <tr>
+                    <th scope="row"><?php echo $info_hotel['name']; ?></th>
+                    <td><?php echo $info_hotel["description"]; ?></td>
+                    <td> <?php echo $info_hotel["vote"]; ?></td>
+                    <td><?php echo $info_hotel["distance_to_center"]; ?></td>
+                </tr>
             <?php } ?>
-        </div>
-
-    <?php } ?>
+        <?php } ?>
+        </tr>
+            </tbody>
+    </div>
 
 </body>
 
